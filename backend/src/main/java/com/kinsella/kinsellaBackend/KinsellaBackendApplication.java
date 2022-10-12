50e74006a16f2fac634ebdb2044eedb42d33cddf
IlyasBaqqari <ilyas.baqqari@and.digital>
2022-10-12 09:57:52 +0100

    added backend skeleton

---
+++ backend/src/main/java/com/kinsella/kinsellaBackend/KinsellaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kinsella.kinsellaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinsellaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinsellaBackendApplication.class, args);
+	}
+
+}
