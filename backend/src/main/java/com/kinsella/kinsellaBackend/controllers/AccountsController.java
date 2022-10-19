b0ed6823a55a4b849af2d1e9b6dd31c119b402e6
Paul Lawlor <paul.lawlor@and.digital>
2022-10-19 16:32:27 +0100

    test commit

--- backend/src/main/java/com/kinsella/kinsellaBackend/controllers/AccountsController.java
+++ backend/src/main/java/com/kinsella/kinsellaBackend/controllers/AccountsController.java
@@ -1,64 +1,37 @@
 package com.kinsella.kinsellaBackend.controllers;
 
 import com.kinsella.kinsellaBackend.model.Accounts;
-import com.kinsella.kinsellaBackend.repositories.AccountsRepository;
+import com.kinsella.kinsellaBackend.repositories.IAccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
+import java.util.Collection;
 import java.util.List;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
-
+@CrossOrigin
 @RestController
-@RequestMapping("jdbc:mariadb://localhost:3306/KD")  //NOTE: ASK ABOUT FILE PATH
 public class AccountsController {
 
     @Autowired
-    private AccountsRepository accountsRepository;
+    private IAccountsRepository accountsRepository;
+
+    //get accounts
+    @GetMapping("/accounts")
+    public List<Accounts> getAccounts() { return accountsRepository.findAll();}
 
     //get account by id
-    @GetMapping("/accounts/{id}")
-    public List<Accounts> getAccountById(@PathVariable Long id) {
-        return accountsRepository.findById(id);
+    @GetMapping("/accounts/{userID}")
+    public Collection<Accounts> getAccountByID(@PathVariable Long id) {
+        return accountsRepository.findByUserID(id);
     }
 
-    //get account, and all data associated, by id
-    @GetMapping("/accounts/{id}")
-    public List<Accounts> getAllAccountById(@PathVariable Long id) {
-        return accountsRepository.findAllById(id);
+    //create a new instance of an account
+    @PostMapping("/accounts")
+    public Accounts createAccount(@Validated @RequestBody Accounts accounts) {
+        accountsRepository.save(accounts);
+        return accountsRepository.save(accounts);
     }
+}
 
-    //get account by firstname
-    @GetMapping("/accounts/{firstName}")
-    public List<Accounts> getAccountByFirstName(@PathVariable String firstName) {
-        return accountsRepository.findByFirstName(firstName);
-    }
 
-    //update account
-     @PatchMapping("/accounts/{id}")
-     public Accounts updateAccount(@PathVariable Long id, @Validated @RequestBody Accounts accountsRequest) {
-         return accountsRepository.findById(id)
-                 .map(accounts -> {
-                     if (accountsRequest.getFirstName() != null) {
-                         accounts.setFirstName(accountsRequest.getFirstName());
-                     }
-                     return accountsRepository.save(accounts);
-                 }).orElseThrow(() -> new EntityNotFoundException("Account not found with id: " + id));
-     }
-
-                    //create a new instance of an account
-                    @PostMapping("/accounts/{id}")
-                    public Accounts createAccount (@Validated @RequestBody Accounts accounts){
-                        return accountsRepository.save(accounts);
-                    }
-
-                    //delete an account and all of it's data by id
-                    @DeleteMapping("/accounts/{id}")
-                    public String deleteAccount (@PathVariable Long id){
-                        accountsRepository.deleteById(id);
-                        return "Account deleted";
-                    }
-                }
--- backend/src/main/java/com/kinsella/kinsellaBackend/repositories/IAccountsRepository.java
+++ backend/src/main/java/com/kinsella/kinsellaBackend/repositories/IAccountsRepository.java
@@ -1,16 +1,11 @@
 package com.kinsella.kinsellaBackend.repositories;
 
 import com.kinsella.kinsellaBackend.model.Accounts;
-import net.bytebuddy.jar.asm.commons.Remapper;
-import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.metamodel.SingularAttribute;
-import java.io.Serializable;
 import java.util.Collection;
-import java.util.List;
 
 
-public interface AccountsRepository extends JpaRepository<Accounts, Integer>{
+public interface IAccountsRepository extends JpaRepository<Accounts, Integer>{
     Collection<Accounts> findByUserID(Long userID);
 }
